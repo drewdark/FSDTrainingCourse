47f6a41c94fb188fb4d1b5441f6e127f20fcb05e
Andrew <andrewedark@gmail.com>
2021-10-06 17:52:27 +0100

    Added functionality to delete component

--- insurance-react-form/src/components/Delete/Delete.js
+++ insurance-react-form/src/components/Delete/Delete.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
 
-const Delete = () => (
-  <div className="Delete">
-    Delete Component
-  </div>
-);
+function Delete() {
 
+  const [id, setID] = useState('');
+
+  const callMockAPI = () => {
+
+    const endpointURL = `https://615d6dee12571a001720760b.mockapi.io/car-insurance/${id}`;
+    axios.delete(endpointURL)
+      .then(alert('Record has been successfully deleted.'))
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div className="Delete">
+
+      <br />
+
+      <Form>
+        <Form.Input
+          fluid
+          id='driverID'
+          label='Driver ID'
+          placeholder='Driver ID'
+          onChange={e => setID(e.target.value)}
+          width={4}
+        />
+        <Button
+          type='submit'
+          onClick={callMockAPI}
+        >Delete</Button>
+      </Form>
+
+    </div>
+  );
+
+
+
+}
 
 export default Delete;
